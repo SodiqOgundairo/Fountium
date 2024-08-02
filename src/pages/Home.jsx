ec746b0df96a47b30b1b5d28c7bb686dcbeba3bc
Sodiq Ogundairo <kingmclamba1@gmail.com>
2024-08-02 22:11:06 +0100

    new hero gif

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,16 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="relative">
-    <div
+    <div className="relative gif-bg">
+    {/* <div
       aria-hidden="true"
       className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
     >
       <div
-        style={{
-          clipPath:
-            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-        }}
+        
         className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
       />
-    </div>
+    </div> */}
     <div className="mx-auto max-w-4xl py-32 sm:py-48 lg:py-56">
       <div className="hidden sm:mb-8 sm:flex sm:justify-center">
         <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -22,14 +19,14 @@ const Home = () => {
         </div>
       </div>
       <div className="text-center">
-        <h1 className=" md:text-7xl font-bold tracking-tight text-gray-900 text-4xl">
+        <h1 className=" md:text-7xl font-bold tracking-tight text-light text-5xl">
          AMAZING EXPERIENCE,
          <span className=" block text-theme">
          EXCELLENCE EVERYDAY
          </span>
         </h1>
       
-        <div className="mt-10 md:flex items-center justify-center gap-x-6">
+        <div className="mt-10 md:flex items-center text-light justify-center gap-x-6">
           <p className="font-bold">Managed IT</p>
           <p className="text-gray-400 font-bold text-lg">|</p>
           <p className="font-bold">Cyber Security</p>
